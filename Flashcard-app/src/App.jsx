2ceb2263c9d04e7be126bd11459f438781cf4078
Franklin Garci <Franklingar2007@gmail.com>
2025-07-26 15:07:58 -0400

    i added the menu button so when ever i press the three dots thats when the add input will appear, i used useeffect and useref. I also styled a little; also added a condtional to the add button so it wouldnt just be adding random flashcards without anything in them

--- Flashcard-app/src/App.jsx
+++ Flashcard-app/src/App.jsx
@@ -1,43 +1,72 @@
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import Card from './Component/Card'
 import './index.css'
+import dotsIcon from './assets/dots.png';
 
 function App() {
    const [flip, setFlip] = useState(false)
-   const [newQuestion, setNewQuestion] = useState('');
-   const[newAnswer, setNewAnswer] = useState('');
+   const [newQuestion, setNewQuestion] = useState(null);
+   const[newAnswer, setNewAnswer] = useState(null);
    const [Index, setIndex] = useState(0)
+   const [add, setadd] = useState(false)
+   const menuRef = useRef(null)
   const [Questions, setQuestion] = useState([
     
     {question:'What is a prop in React', answer:'Data passed to components'}
   ])
 const handleAdd = () => {
-  const newCard = {question: newQuestion, answer:newAnswer}
+  if(newQuestion&& newAnswer){
+ const newCard = {question: newQuestion, answer:newAnswer}
     setQuestion([...Questions, newCard])
     setNewAnswer('')
     setNewQuestion('')
+  }
 }
+useEffect(()=>{
+  const handleClickOutside = (event) =>{
+    if(menuRef.current && !menuRef.current.contains(event.target)){
+      setadd(false);
+    }
+  };
+  document.addEventListener('mousedown', handleClickOutside);
+  return () =>{
+    document.removeEventListener('mousedown', handleClickOutside);
+  };
+}, []);
 console.log(Questions)
   return (
     <>
-    
-       
+      <div className='mCard'>
         <Card 
           key = {Index}
           Question={Questions[Index].question}
           Answer={Questions[Index].answer}
           flip={flip}
+          dotsIcon={dotsIcon}
+          onToggleAdd={() => setadd(prev => !prev)}
         />    
-    
-      <button onClick={() => setFlip(!flip)}> 
+    <div className='Nav'>
+      <button  className = "bNav" onClick={() => setFlip(!flip)}> 
         
         <p> Flip</p>
-        
         </button>
-        <input type = "text" value  = {newQuestion} placeholder='Add new Quesiton' onChange={(e) => setNewQuestion(e.target.value)}/>
+        <button onClick={() => {Index+1<Questions.length?setIndex(Index+1):setIndex(Index)}}> next</button>
+        </div>
+      {add && (
+      <div 
+      ref = {menuRef}
+    > 
+     <input type = "text" value  = {newQuestion} placeholder='Add new Quesiton' onChange={(e) => setNewQuestion(e.target.value)}/>
         <input type = "text" value = {newAnswer} placeholder='Add new Answer' onChange={(e) => setNewAnswer(e.target.value)}/>
         <button onClick={() => handleAdd()}> Add</button>
-        <button onClick={() => {Index+1<Questions.length?setIndex(Index+1):setIndex(Index)}}> next</button>
+
+    </div>
+
+      )}
+    
+
+    </div>
+
     </>
   )
 }
--- Flashcard-app/src/Component/Card.jsx
+++ Flashcard-app/src/Component/Card.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
-const Card = ({Question, Answer,flip }) => {
+const Card = ({Question, Answer,flip, dotsIcon, onToggleAdd }) => {
    
 return(
     <div className='Card'>
+        <div className='Card-info'>
+            <button className='Dbutton'>
+            <img className = "dot" src={dotsIcon} alt = "Menu" onClick={onToggleAdd}/>
+         </button>
+         </div>
+       
         <h1> {flip?Answer:Question}</h1>
     </div>
 )
